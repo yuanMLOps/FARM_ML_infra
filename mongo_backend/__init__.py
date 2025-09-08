ffea65941e60c3d24b210f6248732dedd30600ec
yuanMLOps <yuanaws.2023@gmail.com>
2025-09-08 08:19:23 -0400

    change file locations and imports

--- mongo_backend/__init__.py
+++ mongo_backend/__init__.py
@@ -1,3 +1,4 @@
+from .config import BaseConfig
 from .database import init_db, bulk_insert, insert_compounds
 from .models import Compound, PreparationFormulation, MLFormulation
 from .file_process import construct_formulation_list_from_df, construct_MLformulation_list_from_df, insert_preparation_ML_formulations
