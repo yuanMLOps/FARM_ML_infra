4f1335dc89a4d790da5061cf0c2ebd12eeac8916
yuanMLOps <yuanaws.2023@gmail.com>
2025-09-12 22:33:40 -0400

    add frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+export default function App() {
+  return (
+    <div className="bg-purple-800 text-white min-h-screen p-4 flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-thin">
+        Hello FARM stack!
+      </h1>
+    </div>
+  )
+}
+
