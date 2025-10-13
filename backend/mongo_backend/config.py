987affce425e9a8f8594166fff68586cceb4b061
yuanMLOps <yuanaws.2023@gmail.com>
2025-10-12 20:20:33 -0400

    add K8s deployment code for fastAPI

--- backend/mongo_backend/config.py
+++ backend/mongo_backend/config.py
@@ -15,4 +15,5 @@ class BaseConfig(BaseSettings):
 
     """Loads the dotenv file. Including this is necessary to get
     pydantic to load a .env file."""
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    # model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    model_config = SettingsConfigDict(extra="ignore")
